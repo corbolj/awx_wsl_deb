e1f075f078f57b6c607c05188affe31bfe29d7a9
Louie Corbo <corbolj@gmail.com>
2025-01-20 10:53:19 -0500

    Improved the initilzation script.

--- run.sh
+++ run.sh
@@ -2,10 +2,17 @@
 
 # Install Git
 if ! which git &> /dev/null
+then
     sudo apt install git -y
-if
+fi
 
- git clone https://github.com/corbolj/awx_wsl_deb.git
+# Stage playbook
+if [ ! -f $HOME/projects/github.com/$USER/awx_wsl_deb/run.sh ]
+then
+    mkdir -p $HOME/projects/github.com/$USER/
+    git clone https://github.com/corbolj/awx_wsl_deb.git $HOME/projects/github.com/$USER/awx_wsl_deb
+    cd $HOME/projects/github.com/$USER/awx_wsl_deb
+fi
 
 # Install ansible
 if ! which ansible-playbook &> /dev/null
