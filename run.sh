bc23fa2d1d535bbe0ef82cd6d1f76e639d3358f5
Louie Corbo <corbolj@gmail.com>
2025-01-20 10:00:20 -0500

    init commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install Git
+if ! which git &> /dev/null
+    sudo apt install git -y
+if
+
+ git clone https://github.com/corbolj/awx_wsl_deb.git
+
+# Install ansible
+if ! which ansible-playbook &> /dev/null
+then
+    UBUNTU_CODENAME=jammy
+    wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+    sudo apt update 
+    sudo apt install ansible -y
+fi
+
+# Execute playbook
+ansible-playbook -v --ask-become-pass playbook.yml
